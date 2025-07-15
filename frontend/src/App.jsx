816e51e70d45351dc2c782713bc74ac557d66fcd
AAYUSH CHAUHAN <97545329+08-ayush@users.noreply.github.com>
2025-07-15 23:48:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="https://chat-app-v5vu.onrender.com"
+export const serverUrl="https://chat-app-7hv9.onrender.com"
 function App() {
    getCurrentUser()
    getSuggestedUsers()
